00c16fa2a1d9b22734c1f30705cb09a7ae3ee014
Jayanta66 <jayhalder18@gmail.com>
2025-02-15 12:21:36 +0530

    add

--- src/main/java/com/example/nikhilbharatV3/NikhilbharatV3Application.java
+++ src/main/java/com/example/nikhilbharatV3/NikhilbharatV3Application.java
@@ -6,29 +6,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NikhilbharatV3Application {
 
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(NikhilbharatV3Application.class, args);
-		//hellow       
-			//server name : ssss
-		
-		//	admin Userame : nbbssssss
-		//	admin password: HF9K#aR!wJbo
-		
-	//	user name for cpanel : nikhilbharat
-	//	password : %LauxvL#-ZDZ
+		// hellow
+		// server name : ssss
 
+		// admin Userame : nbbssssss
+		// admin password: HF9K#aR!wJbo
+
+		// user name for cpanel : nikhilbharat
+		// password : %LauxvL#-ZDZ
 
 //	ns1.160-153-175-154.cprapid.com
 // ns2.160-153-175-154.cprapid.com
-		
-	//	cpanel access without domain : http://97.74.88.23/~nikhilbharat/
-		
+
+		// cpanel access without domain : http://97.74.88.23/~nikhilbharat/
 
 //lisa.ns.cloudflare.com
 //sean.ns.cloudflare.com
-		
+
 	}
 
 }

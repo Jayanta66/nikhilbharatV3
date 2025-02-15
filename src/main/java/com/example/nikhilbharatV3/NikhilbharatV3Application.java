97e374329aaddfc61dd7dd5db7ade2ea7e54f4ba
Jayanta66 <jayhalder18@gmail.com>
2025-02-15 09:59:12 +0530

    add

--- src/main/java/com/example/nikhilbharatV3/NikhilbharatV3Application.java
+++ src/main/java/com/example/nikhilbharatV3/NikhilbharatV3Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NikhilbharatV3Application {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NikhilbharatV3Application.class, args);
 		//hellow       
